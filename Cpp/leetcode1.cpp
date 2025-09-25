8b98f62cb54f04fe9ba79ee8023696352535b6bf
Վահե Բաբայան <vahebabayan.tt219-2@polytechnic.am>
2025-09-25 11:05:57 +0400

    leetcode 1. Two Sum

--- Cpp/leetcode1.cpp
+++ Cpp/leetcode1.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <unordered_map>
 
+//1. Two Sum
 class Solution {
 public:
     std::vector<int> twoSum(std::vector<int>& nums, int target) {
@@ -19,12 +20,12 @@ public:
 int main() {
     Solution sol;
     std::vector<int> nums1 = {2,7,11,15};
-    std::vector<int> res1 = sol.twoSum(nums1, 9); // [0,1]
-    std::cout << res1[0] << "," << res1[1] << std::endl;
+    std::vector<int> res1 = sol.twoSum(nums1, 9);
+    std::cout << res1[0] << "," << res1[1] << std::endl; // [0,1]
     std::vector<int> nums2 = {3,2,4};
-    std::vector<int> res2 = sol.twoSum(nums2, 6); // [1,2]
-    std::cout << res2[0] << "," << res2[1] << std::endl;
+    std::vector<int> res2 = sol.twoSum(nums2, 6);
+    std::cout << res2[0] << "," << res2[1] << std::endl; // [1,2]
     std::vector<int> nums3 = {3,3};
-    std::vector<int> res3 = sol.twoSum(nums3, 6); // [0,1]
-    std::cout << res3[0] << "," << res3[1] << std::endl;
+    std::vector<int> res3 = sol.twoSum(nums3, 6);
+    std::cout << res3[0] << "," << res3[1] << std::endl; // [0,1]
 }
