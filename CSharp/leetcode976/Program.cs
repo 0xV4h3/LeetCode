15e515955525e0af52fc257bd173c853f62ab9a6
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-09-28 13:37:04 +0400

    leetcode 976. Largest Perimeter Triangle

---
+++ CSharp/leetcode976/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace leetcode976
+{
+    //976. Largest Perimeter Triangle
+    public class Solution
+    {
+        public int LargestPerimeter(int[] nums)
+        {
+            Array.Sort(nums);
+            for (int i = nums.Length - 1; i >= 2; i--)
+            {
+                if (nums[i - 2] + nums[i - 1] > nums[i])
+                {
+                    return nums[i - 2] + nums[i - 1] + nums[i];
+                }
+            }
+            return 0;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.LargestPerimeter([2, 1, 2])); // 5
+            Console.WriteLine(sol.LargestPerimeter([1, 2, 1, 10])); // 0
+        }
+    }
+}
