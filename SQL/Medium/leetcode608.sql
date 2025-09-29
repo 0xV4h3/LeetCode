bfa0e60c5ad21155ec797fcab10fa6fa342f36c2
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-09-29 19:36:30 +0400

    leetcode 608. Tree Node

---
+++ SQL/Medium/leetcode608.sql
@@ -0,0 +1,12 @@
+-- 608. Tree Node
+SELECT `id`, 
+CASE 
+    WHEN `p_id` IS NULL THEN "Root"
+    WHEN `id` IN (
+        SELECT DISTINCT `p_id`
+        FROM Tree
+        WHERE `p_id` IS NOT NULL
+    ) THEN "Inner"
+    ELSE "Leaf"
+    END `type`
+FROM Tree
