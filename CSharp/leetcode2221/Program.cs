d32eb7f2fe67c974c478c7d66096a55e08c0731d
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-09-29 20:20:48 +0400

    leetcode 2221. Find Triangular Sum of an Array

---
+++ CSharp/leetcode2221/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace leetcode2221
+{
+    //2221. Find Triangular Sum of an Array
+    public class Solution
+    {
+        public int TriangularSum(int[] nums)
+        {
+            int[] newNums = (int[])nums.Clone();
+            int length = newNums.Length;
+
+            while (length != 1)
+            {
+                for (int i = 0; i < length - 1; i++)
+                {
+                    newNums[i] = (newNums[i] + newNums[i + 1]) % 10;
+                }
+                length--;
+            }
+            return newNums[0];
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.TriangularSum([1, 2, 3, 4, 5])); // 8
+            Console.WriteLine(sol.TriangularSum([5])); // 5
+        }
+    }
+}
