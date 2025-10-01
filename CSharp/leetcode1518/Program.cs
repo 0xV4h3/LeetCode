dc8171cc82e49f6bb707f8ea5dc2b4106b5cabeb
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-10-01 09:20:56 +0400

    leetcode 1518. Water Bottles

---
+++ CSharp/leetcode1518/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace leetcode1518
+{
+    //1518. Water Bottles
+    public class Solution
+    {
+        public int NumWaterBottles(int numBottles, int numExchange)
+        {
+            return numBottles + (numBottles - 1) / (numExchange-1);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.NumWaterBottles(numBottles:9, numExchange:3)); // 13
+            Console.WriteLine(sol.NumWaterBottles(numBottles:15, numExchange:4)); // 19
+        }
+    }
+}
