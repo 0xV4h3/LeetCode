fb99061e7c1505b6d0cf29494ce944a483bf669e
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-10-02 11:14:20 +0400

    leetcode 3100. Water Bottles II

---
+++ CSharp/leetcode3100/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode3100
+{
+    internal class Program
+    {
+        //3100. Water Bottles II
+        public class Solution
+        {
+            public int MaxBottlesDrunk(int numBottles, int numExchange)
+            {
+                int maxDrunk = numBottles;
+                while (numBottles >= numExchange)
+                {
+                    numBottles -= numExchange - 1;
+                    numExchange++;
+                    maxDrunk++;
+                }
+                return maxDrunk;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.MaxBottlesDrunk(numBottles: 13, numExchange: 6)); // 15
+            Console.WriteLine(sol.MaxBottlesDrunk(numBottles: 10, numExchange: 3)); // 13
+        }
+    }
+}
