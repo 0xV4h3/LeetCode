6c9ec72f8e3fecee75a0d925962a52ef5253595b
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-10-20 19:03:11 +0400

    leetcode 2011. Final Value of Variable After Performing Operations

---
+++ CSharp/leetcode2011/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode2011
+{
+    //2011. Final Value of Variable After Performing Operations
+    public class Solution
+    {
+        public int FinalValueAfterOperations(string[] operations)
+        {
+            int x = 0;
+            foreach (string operation in operations)
+            {
+                if (operation == "X++" || operation == "++X") x++;
+                else x--;
+            }
+            return x;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.FinalValueAfterOperations(["--X", "X++", "X++"])); // 1
+            Console.WriteLine(sol.FinalValueAfterOperations(["++X", "++X", "X++"])); // 3
+            Console.WriteLine(sol.FinalValueAfterOperations(["X++", "++X", "--X", "X--"])); // 0
+        }
+    }
+}
