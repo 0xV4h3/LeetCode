f8d9702bd939dba7a2e11f893bd2b98f5312e278
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-10-25 14:53:34 +0400

    leetcode 1716. Calculate Money in Leetcode Bank

---
+++ CSharp/leetcode1716/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace leetcode1716
+{
+    //1716. Calculate Money in Leetcode Bank
+    public class Solution
+    {
+        public int TotalMoney(int n)
+        {
+            int totalMoney = 0;
+            int week = 1;
+            while (n > 0)
+            {
+                int days = n >= 7 ? 7 : n;
+                totalMoney += (days * (2 * week + (days - 1))) / 2;
+                n -= days;
+                week++;
+            }
+            return totalMoney;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.TotalMoney(n: 4)); // 10
+            Console.WriteLine(sol.TotalMoney(n: 10)); // 37
+            Console.WriteLine(sol.TotalMoney(n: 20)); // 96
+        }
+    }
+}
