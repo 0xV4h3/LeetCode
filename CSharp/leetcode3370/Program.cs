e959c997bd0df23f4bbdc91086ff2e3eaefa3c2c
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-10-29 19:16:39 +0400

    leetcode 3370. Smallest Number With All Set Bits

---
+++ CSharp/leetcode3370/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace leetcode3370
+{
+    //3370. Smallest Number With All Set Bits
+    public class Solution
+    {
+        public int SmallestNumber(int n)
+        {
+            return (int)Math.Pow(2, (int)Math.Log2(n) + 1) - 1;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.SmallestNumber(n: 5)); // 7
+            Console.WriteLine(sol.SmallestNumber(n: 10)); // 15
+            Console.WriteLine(sol.SmallestNumber(n: 3)); // 3
+        }
+    }
+}
