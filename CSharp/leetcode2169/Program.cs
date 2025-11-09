3c1b4dde269cccaea7e170530f6379c3ba8375e3
Vahe Babayan <vahe.babayan.dev@gmail.com>
2025-11-09 16:50:54 +0400

    leetcode 2169. Count Operations to Obtain Zero

---
+++ CSharp/leetcode2169/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace leetcode2169
+{
+    //2169. Count Operations to Obtain Zero
+    public class Solution
+    {
+        public int CountOperations(int num1, int num2)
+        {
+            int count = 0;
+            while (num1 != 0 && num2 != 0)
+            {
+                count += num1 / num2;
+                num1 %= num2;
+                (num1, num2) = (num2, num1);
+            }
+            return count;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+            Console.WriteLine(sol.CountOperations(2, 3)); // 3
+            Console.WriteLine(sol.CountOperations(10, 10)); // 1
+        }
+    }
+}
